3ea8db1355dd1f6d43d8fcfe93c1ec35835950e6
Gregory1234 <Gregory1234@users.noreply.github.com>
2019-07-28 23:31:12 +0200

    Added AST

---
+++ Makefile
@@ -0,0 +1,4 @@
+
+
+all:
+	cabal new-run GregLang -- main.gl
---
+++ src/GL/Treeable.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE FlexibleInstances #-}
+module GL.Treeable where
+
+import Data.Tree
+
+class Treeable a where
+  toTree :: a -> Tree String
+
+listToTree :: Treeable a => String -> [a] -> Tree String
+listToTree s = Node s . map toTree
+
+instance Treeable String where
+  toTree s = Node s []
+
+treeShow :: Treeable a => a -> String
+treeShow = drawTree . toTree
