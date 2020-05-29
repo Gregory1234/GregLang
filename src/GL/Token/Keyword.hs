f61a1cb62830248a41d2bfe6bbca49b5e31c8987
Gregory1234 <Gregory1234@users.noreply.github.com>
2020-05-29 21:21:10 +0200

    Implemented the keyword lexer

--- src/GL/Token/Keyword.hs
+++ src/GL/Token/Keyword.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE FlexibleInstances #-}
 module GL.Token.Keyword
   ( module GL.Token.Keyword
   )
@@ -6,59 +8,115 @@ where
 import qualified Text.ParserCombinators.ReadP  as RP
 import           Text.Read               hiding ( Ident )
 import           Data.String
+import           Data.Functor
+import           Data.Char
+import           Data.Bool
+import           GL.Utils
 import           GL.Lexer.Lexable
 
-data Operator = Add | Sub | Mul | Div | And | Mod | Or | XOr | BAnd | BOr | BXor
-  deriving (Eq, Ord)
+funToLexable :: (Enum a, Bounded a) => (a -> String) -> ReadPrec a
+funToLexable f = lift $ RP.choice (map (\x -> RP.string (f x) $> x) enumerate)
+
+data Operator = Add | Sub | Mul | Div | Mod | And | Or | XOr | BAnd | BOr | BXOr
+  deriving (Eq, Ord, Show, Read, Enum, Bounded)
+
+getOperator :: Operator -> String
+getOperator Add  = "+"
+getOperator Sub  = "-"
+getOperator Mul  = "*"
+getOperator Div  = "/"
+getOperator Mod  = "%"
+getOperator And  = "&&"
+getOperator Or   = "||"
+getOperator XOr  = "^^"
+getOperator BAnd = "&"
+getOperator BOr  = "|"
+getOperator BXOr = "^"
 
 instance Lexable Operator where
-  lexAP = lift $ RP.choice []
+  lexAP = funToLexable getOperator
 
-data Comparasion = Eq | NEq | Gt | Lt | GEq | LEq
-  deriving (Eq, Ord)
+data Comparasion = Eq | NEq | GEq | LEq | Gt | Lt
+  deriving (Eq, Ord, Show, Read, Enum, Bounded)
+
+getComparasion :: Comparasion -> String
+getComparasion Eq  = "=="
+getComparasion NEq = "!="
+getComparasion Gt  = ">"
+getComparasion Lt  = "<"
+getComparasion GEq = ">="
+getComparasion LEq = "<="
 
 instance Lexable Comparasion where
-  lexAP = lift $ RP.choice []
+  lexAP = funToLexable getComparasion
 
-data OtherOperator = Not | Inc | Dec
-  deriving (Eq, Ord)
+data OtherOperator = Not | BNot | Inc | Dec
+  deriving (Eq, Ord, Show, Read, Enum, Bounded)
+
+getOtherOperator :: OtherOperator -> String
+getOtherOperator Not  = "!"
+getOtherOperator BNot = "~"
+getOtherOperator Inc  = "++"
+getOtherOperator Dec  = "--"
 
 instance Lexable OtherOperator where
-  lexAP = lift $ RP.choice []
+  lexAP = funToLexable getOtherOperator
 
-data ControlStructure = If | For | While | Do | Switch | Break | Continue
-  deriving (Eq, Ord)
+data ReservedKeyword
+  = If | For | While | Do | Switch
+  | Break | Continue
+  | Let
+  | Package | Import | Class
+  deriving (Eq, Ord, Show, Read, Enum, Bounded)
 
-instance Lexable ControlStructure where
-  lexAP = lift $ RP.choice []
+getReservedKeyword :: ReservedKeyword -> String
+getReservedKeyword = map toLower . show
+
+instance Lexable ReservedKeyword where
+  lexAP = funToLexable getReservedKeyword
 
 data BracketType = Bracks | Parens | Braces
-  deriving (Eq, Ord)
+  deriving (Eq, Ord, Show, Read, Enum, Bounded)
 
-instance Lexable BracketType where
-  lexAP = lift $ RP.choice []
+getBracket :: BracketType -> (String, String)
+getBracket Bracks = ("[", "]")
+getBracket Parens = ("(", ")")
+getBracket Braces = ("{", "}")
 
-data GlobalKeyword = Package | Import | Class
-  deriving (Eq, Ord)
+type Bracket = (BracketType, Bool)
 
-instance Lexable GlobalKeyword where
-  lexAP = lift $ RP.choice []
+instance Lexable Bracket where
+  lexAP = helper False fst <|> helper True snd
+    where helper b f = (, b) <$> funToLexable (f . getBracket)
 
 data Keyword
   = OKeyword Operator
   | OOKeyword OtherOperator
-  | OSKeyword Operator
+  | OSKeyword (Maybe Operator)
   | CKeyword Comparasion
-  | CSKeyword ControlStructure
-  | BKeyword BracketType Bool
-  | GKeyword GlobalKeyword
+  | RKeyword ReservedKeyword
+  | BKeyword Bracket
   deriving (Eq, Ord)
 
 instance Lexable Keyword where
-  lexAP = lift $ RP.choice []
+  lexAP = choice
+    [ CKeyword <$> lexAP
+    , OSKeyword . Just <$> (lexAP <* lift (RP.char '='))
+    , OSKeyword <$> (lift (RP.char '=') $> Nothing)
+    , OKeyword <$> lexAP
+    , OOKeyword <$> lexAP
+    , BKeyword <$> lexAP
+    , RKeyword <$> lexAP
+    ]
 
 instance IsString Keyword where
   fromString _ = undefined
 
 getKeyword :: Keyword -> String
-getKeyword _ = "TODO"
+getKeyword (OKeyword  x       ) = getOperator x
+getKeyword (OOKeyword x       ) = getOtherOperator x
+getKeyword (OSKeyword (Just x)) = getOperator x ++ "="
+getKeyword (OSKeyword Nothing ) = "="
+getKeyword (CKeyword  x       ) = getComparasion x
+getKeyword (RKeyword  x       ) = getReservedKeyword x
+getKeyword (BKeyword  (x, b)  ) = bool fst snd b $ getBracket x
